25b36e2c2788347d42b0c73e093f0c7f7037a1ec
Jahia CI <jahia-ci@jahia.com>
2011-06-04 14:00:57 +0000

    [maven-release-plugin] prepare release JAHIA_6_5_0_0

---
+++ src/main/webapp/WEB-INF/macros/username.groovy
@@ -0,0 +1,12 @@
+if (currentUser.username.trim().equals("guest")) {
+    print "guest"
+} else {
+    String property1 = currentUser.getProperty("j:firstName")
+    if (property1 != null)
+        print(property1.capitalize() + " ");
+    String property2 = currentUser.getProperty("j:lastName")
+    if (property2 != null)
+        print(property2.capitalize())
+    if (property1 == null && property2 == null)
+        print(currentUser.getUsername().capitalize())
+}
