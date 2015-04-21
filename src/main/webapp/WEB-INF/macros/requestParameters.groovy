cbd8a7a76cec29c935173f2312ea25cde2e7c6aa
fabrice aissah <faissah@jahia.com>
2015-04-21 14:42:55 -0400

    Rollback https://devtools.jahia.com/fisheye/cru/github-macros-1 while releasing

--- src/main/webapp/WEB-INF/macros/requestParameters.groovy
+++ src/main/webapp/WEB-INF/macros/requestParameters.groovy
@@ -1,29 +1,3 @@
-def printEncodedString(parameter){
-    if(parameter.contains("%25")){
-        def values = parameter.split('%')
-        for(param in values[0..<values.size()-1]){
-            print URLEncoder.encode(param, "UTF-8")
-            print "%"
-        }
-        print URLEncoder.encode(values[values.size()-1], "UTF-8")
-    }
-    else{
-        print URLEncoder.encode(parameter, "UTF-8")
-    }
-}
-
 if (renderContext.getRequest().getQueryString() != null) {
-    print "?"
-    def paramsList = renderContext.getRequest().getQueryString().split('&')
-    for(param in paramsList[0..<paramsList.size()-1]){
-        def (paramName, value) = param.tokenize( '=' )
-        printEncodedString(paramName)
-        print "="
-        printEncodedString(value)
-        print "&"
-    }
-    def (paramName, value) = paramsList[paramsList.size()-1].tokenize( '=' )
-    printEncodedString(paramName)
-    print "="
-    printEncodedString(value)
+print "?" + URLEncoder.encode(renderContext.getRequest().getQueryString(), "UTF-8")
 }
