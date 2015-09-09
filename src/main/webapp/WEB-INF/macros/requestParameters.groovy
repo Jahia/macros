a22931ccf31fa1f154ff67100e55b7eee5c49b06
wassek <werner@jahia.com>
2015-09-09 11:46:24 +0200

    LACAP-176: Request parameters in language switching links get too much urlEncode

--- src/main/webapp/WEB-INF/macros/requestParameters.groovy
+++ src/main/webapp/WEB-INF/macros/requestParameters.groovy
@@ -1,3 +1,3 @@
 if (renderContext.getRequest().getQueryString() != null) {
-print "?" + URLEncoder.encode(renderContext.getRequest().getQueryString(), "UTF-8")
+print "?" + URLEncoder.encode(URLDecoder.decode(renderContext.getRequest().getQueryString(), "UTF-8"), "UTF-8")
 }
