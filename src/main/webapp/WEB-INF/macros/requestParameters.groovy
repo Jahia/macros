dd5bc705f618c40b0596ce09069376f70b4125b3
David Griffon <dgriffon@jahia.com>
2015-10-06 00:17:57 +0200

    DEVNSI-14 : encode only when necessary

--- src/main/webapp/WEB-INF/macros/requestParameters.groovy
+++ src/main/webapp/WEB-INF/macros/requestParameters.groovy
@@ -1,3 +1,25 @@
-if (renderContext.getRequest().getQueryString() != null) {
-print "?" + URLEncoder.encode(URLDecoder.decode(renderContext.getRequest().getQueryString(), "UTF-8"), "UTF-8")
+def queryString = renderContext.getRequest().getQueryString()
+
+if (queryString != null) {
+    if ("true".equals(renderContext.getRequest().getParameter("encoded_querystring"))) {
+        print "?" + queryString;
+    } else {
+        def paramsList = queryString.split('&')
+        def encoded = false;
+        paramsList.eachWithIndex { param, index ->
+            print index < 1 ? "?" : "&"
+            def (paramName, value) = param.tokenize('=')
+            if (paramName != null) {
+                print URLEncoder.encode(paramName, "UTF-8")
+                encoded = true;
+                if (value != null) {
+                    print "="
+                    print URLEncoder.encode(value, "UTF-8")
+                }
+            }
+        }
+        if (encoded) {
+            print "&encoded_querystring=true"
+        }
+    }
 }
