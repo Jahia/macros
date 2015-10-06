48f266bb3b9e21240395a1464df262c92b543b9f
Thomas Draier <tdraier@jahia.com>
2015-10-06 15:14:42 +0200

    Revert all changes

--- src/main/resources/WEB-INF/macros/requestParameters.groovy
+++ src/main/resources/WEB-INF/macros/requestParameters.groovy
@@ -1,25 +1,3 @@
-def queryString = renderContext.getRequest().getQueryString()
-
-if (queryString != null) {
-    if ("true".equals(renderContext.getRequest().getParameter("encoded_querystring"))) {
-        print "?" + queryString;
-    } else {
-        def paramsList = queryString.split('&')
-        def encoded = false;
-        paramsList.eachWithIndex { param, index ->
-            print index < 1 ? "?" : "&"
-            def (paramName, value) = param.tokenize('=')
-            if (paramName != null) {
-                print URLEncoder.encode(paramName, "UTF-8")
-                encoded = true;
-                if (value != null) {
-                    print "="
-                    print URLEncoder.encode(value, "UTF-8")
-                }
-            }
-        }
-        if (encoded) {
-            print "&encoded_querystring=true"
-        }
-    }
+if (renderContext.getRequest().getQueryString() != null) {
+print "?" + renderContext.getRequest().getQueryString()
 }
