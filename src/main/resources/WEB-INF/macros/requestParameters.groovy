bd0267dea3a33eb79ef3aa8062bc5cf451c69dce
bpapez <bpapez@jahia.com>
2015-11-10 10:49:39 +0100

    QA-7972: Fix query string forwarding on Websphere

--- src/main/resources/WEB-INF/macros/requestParameters.groovy
+++ src/main/resources/WEB-INF/macros/requestParameters.groovy
@@ -1,3 +1,5 @@
 if (renderContext.getRequest().getQueryString() != null) {
-print "?" + renderContext.getRequest().getQueryString()
+    print "?" + renderContext.getRequest().getQueryString()
+} else if (renderContext.getRequest().getAttribute("javax.servlet.forward.query_string") != null) {
+    print "?" + renderContext.getRequest().getAttribute("javax.servlet.forward.query_string")
 }
