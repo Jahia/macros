2e24c69a16dc2f86d5afbb3cdfea3c9c26b8cb5e
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2025-08-07 18:29:25 -0400

    Sanitize error handling messages (#38)

--- src/main/resources/WEB-INF/modules-macros/resourceBundle.groovy
+++ src/main/resources/WEB-INF/modules-macros/resourceBundle.groovy
@@ -1,10 +1,11 @@
 import org.jahia.utils.i18n.JahiaResourceBundle
 
 if(binding.variables.containsKey("param1")){
-    try{
-        print JahiaResourceBundle.getString(binding.variables.containsKey("param2") ? param2 : null, param1, renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName());
-    }catch(java.util.MissingResourceException e){
-        print param1;
+    try {
+        def bundleName = binding.variables.containsKey("param2") ? param2 : null;
+        print JahiaResourceBundle.getString(bundleName, param1, renderContext.getMainResourceLocale(), renderContext.getSite().getTemplatePackageName());
+    } catch(java.util.MissingResourceException e) {
+        print org.apache.commons.lang.StringEscapeUtils.escapeXml(param1);
     }
 }else{
     print "<p>This macro require one or two parameter like : <br />" +
