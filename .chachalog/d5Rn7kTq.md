24622fbf85ea9f902c985250ca0c0d95e36e179c
Kevan <kevan@jahia.com>
2026-07-08 16:44:35 +0200

    fix: escape keyword values rendered by the keywords macro

---
+++ .chachalog/d5Rn7kTq.md
@@ -0,0 +1,6 @@
+---
+# Allowed version bumps: patch, minor, major
+macros: patch
+---
+
+Fixed display of keyword values containing special characters rendered by the keywords macro.
--- src/main/resources/WEB-INF/modules-macros/keywords.groovy
+++ src/main/resources/WEB-INF/modules-macros/keywords.groovy
@@ -8,7 +8,7 @@ if(currentNode.hasProperty("j:keywords")){
     keywords = currentNode.getProperty("j:keywords").getValues();
     keywordsSize =  keywords.size();
     for(int i = 0 ; i < keywordsSize ; i++){
-        print keywords.value[i].getString();
+        print org.apache.commons.lang.StringEscapeUtils.escapeXml(keywords.value[i].getString());
         if(keywordsSize > 0 && i < keywordsSize-1){
             print ", ";
         }
