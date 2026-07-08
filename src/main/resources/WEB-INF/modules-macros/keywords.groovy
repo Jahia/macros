a5beef6c88bf2d47642e223826f73840a7f55007
Geofrey Flores <gflores@jahia.com>
2026-07-08 14:41:32 -0400

    fix: Escape keywords rendering

--- src/main/resources/WEB-INF/modules-macros/keywords.groovy
+++ src/main/resources/WEB-INF/modules-macros/keywords.groovy
@@ -1,20 +1,14 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Damien GAILLARD
- * Date: 11/8/13
- * Time: 9:28 AM
- */
+import org.apache.commons.lang.StringEscapeUtils
+
 if(currentNode.hasProperty("j:keywords")){
     keywords = currentNode.getProperty("j:keywords").getValues();
     keywordsSize =  keywords.size();
     for(int i = 0 ; i < keywordsSize ; i++){
-        print keywords.value[i].getString();
+        print StringEscapeUtils.escapeHtml(keywords.value[i].getString());
         if(keywordsSize > 0 && i < keywordsSize-1){
             print ", ";
         }
     }
-}else{
+} else {
     print "Defined Keyword(s) before use !";
 }
-
-
